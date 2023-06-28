b2e29f8d3a05cbcf1023c54703f1e249f6d4f95e
Vibha Sengar <vibhasengar03@gmail.com>
2023-06-28 18:41:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (99.53%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0){
+        long ne =n;
+        if(ne==0){
             return false;
         }
-        if(n==1){
+        if(ne==1){
             return true;
         }
-        else{
-            while(n!=1){
-                if(n%2!=0){
-                    return false;
-                }
-                n=n/2;
-            }
+        if((ne&(ne-1))==0){
+            return true;
         }
-        return true;
+        return false;
         
     }
 };
