903097a218148fb9d19d74a7e82045d55cf2abd5
Vibha Sengar <vibhasengar03@gmail.com>
2023-06-30 00:50:27 +0530

    Time: 7 ms (26.54%), Space: 5.9 MB (45.40%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numberOfMatches(int n) 
+    {
+        int count=0;
+        int temp =n;
+        int match =0;
+        while(temp!=1)
+        {
+            if(temp%2==0)
+            {
+                match = temp/2;
+                cout<<"Match"<<match<<endl;
+                temp = temp/2;
+                cout<<"Advance"<<temp<<endl;
+            }
+            else
+            {
+                match=(temp-1)/2;
+                cout<<"Match"<<match<<endl;
+                temp = ((temp-1)/2)+1;
+                cout<<"Advance"<<temp<<endl;
+            }
+            count+=match;
+        }
+        return count;
+    }
+};
