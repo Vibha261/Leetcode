9e08530a0935f33a7d748371856b2d777193c0e4
Vibha Sengar <vibhasengar03@gmail.com>
2023-11-04 00:47:56 +0530

    Time: 15 ms (62.74%), Space: 17.2 MB (68.93%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int i;
-        for(auto it: nums){
-            i=count(nums.begin(),nums.end(),it);
-            if(i==1){
-                i=it;
-                break;
-            }
+//         int i;
+//         for(auto it: nums){
+//             i=count(nums.begin(),nums.end(),it);
+//             if(i==1){
+//                 i=it;
+//                 break;
+//             }
             
+//         }
+//         return i;
+        int xorsum=0;
+        int n=nums.size();
+        for(int i=0; i<n; i++)
+        {
+            xorsum = xorsum^nums[i];
         }
-        return i;
-        
+        return xorsum;
     }
 };
